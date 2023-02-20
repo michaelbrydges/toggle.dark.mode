0d4cb1f667c18969b69b4c543e563a42fe5e383b
michaelbrydges <michael.brydges@edu.sait.ca>
2023-02-19 17:58:39 -0700

    update comments

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// GIF in link below
-// https://dribbble.com/shots/5846239-Light-dark-toggle-switch-InVision-Studio 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
